1abd70cb9e449d095f59aa8416a530a26115f689
Kranium Mendoza <github@kranium.au>
2026-07-26 13:17:48 +1000

    feat: drop redirects

--- cells/site/nixosModules.nix
+++ cells/site/nixosModules.nix
@@ -1,39 +1,14 @@
 let
   currentSite = "kranium.au";
-  legacySite = "gikos.net";
-  testCF = "kranium.net";
 in
 {
   default.services.nginx.virtualHosts = {
-    "${testCF}" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/".root = inputs.cells.site.packages.site-generated;
-    };
     "${currentSite}" = {
       forceSSL = true;
       enableACME = true;
       locations."/".root = inputs.cells.site.packages.site-generated;
     };
-    "${legacySite}" = {
-      forceSSL = true;
-      enableACME = true;
-      locations = {
-        # keep the old posts available but dont redirect
-        "/posts" = {
-          root = inputs.cells.site.packages.site-generated;
-          extraConfig = ''
-            try_files $uri $uri/ =404;
-          '';
-        };
-        "/" = {
-          extraConfig = ''
-            return 301 https://${currentSite}$request_uri;
-          '';
-        };
-      };
-    };
-    "www.${legacySite}" = {
+    "www.${currentSite}" = {
       forceSSL = true;
       enableACME = true;
       globalRedirect = currentSite;
